c802ea22370248f30c416c4406313931377f59e2
Xi CHEN <chenxifr@gmail.com>
2015-01-09 00:31:25 +0100

    clean

--- src/main/java/com/seanchenxi/gwt/storage/test/server/GreetingServiceImpl.java
+++ src/main/java/com/seanchenxi/gwt/storage/test/server/GreetingServiceImpl.java
@@ -1,19 +1,13 @@
 package com.seanchenxi.gwt.storage.test.server;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
-import com.google.gwt.user.server.rpc.SerializationPolicy;
-import com.google.gwt.user.server.rpc.SerializationPolicyLoader;
 import com.seanchenxi.gwt.storage.shared.StorageUtils;
 import com.seanchenxi.gwt.storage.test.client.GreetingService;
 import com.seanchenxi.gwt.storage.test.shared.FieldVerifier;
 import com.seanchenxi.gwt.storage.test.shared.TestValue;
 
 import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.text.ParseException;
 import java.util.Random;
 
 /**
@@ -75,44 +69,5 @@ public class GreetingServiceImpl extends RemoteServiceServlet implements
     }
     return html.replaceAll("&", "&amp;").replaceAll("<", "&lt;").replaceAll(
         ">", "&gt;");
-  }
-
-  public static SerializationPolicy loadSerializationPolicy(ServletContext context, String moduleName) {
-    if(true) {
-      String contextRelativePath = "/" + moduleName + "/" + "StorageSerializerPolicy";
-      context.log(contextRelativePath);
-      String serializationPolicyFilePath = SerializationPolicyLoader.getSerializationPolicyFileName(contextRelativePath);
-      context.log(serializationPolicyFilePath);
-      InputStream is = context.getResourceAsStream(serializationPolicyFilePath);
-
-      try {
-        if(is != null) {
-          try {
-            return loadSerializationPolicy(is, moduleName, true);
-          } catch (ParseException var18) {
-            context.log("ERROR: Failed to parse the policy file \'" + serializationPolicyFilePath + "\'", var18);
-          } catch (IOException var19) {
-            context.log("ERROR: Could not read the policy file \'" + serializationPolicyFilePath + "\'", var19);
-          }
-        } else {
-          String e = "ERROR: The serialization policy file \'" + serializationPolicyFilePath + "\' was not found; did you forget to include it in this deployment?";
-          context.log(e);
-        }
-      } finally {
-        if(is != null) {
-          try {
-            is.close();
-          } catch (IOException var17) {
-          }
-        }
-
-      }
-
-    }
-    return null;
-  }
-
-  public static SerializationPolicy loadSerializationPolicy(InputStream is, String moduleName, boolean isDefault) throws IOException, ParseException {
-    return SerializationPolicyLoader.loadFromStream(is, null);
   }
 }
